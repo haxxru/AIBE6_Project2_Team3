971e8a26e2cfd431984f6f0ad0052308d59b3ca4
Unordinary <92520481+Unord1nary@users.noreply.github.com>
2026-04-13 17:28:43 +0900

    [Feat] 게시글 데이터 전용 Article 클래스 생성

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,4 +1,39 @@
 package org.example;
 
 public class Article {
+    private int id;
+    private String title;
+    private String content;
+    private String reqDate;
+    
+    public Article(int id, String title, String content, String reqDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.reqDate = reqDate;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
+
